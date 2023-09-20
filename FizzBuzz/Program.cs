59042af62fc3404dd787d5bbabd5e4e33fb27f53
Mark Roberts <Mark.Roberts@softwire.com>
2023-09-20 10:15:51 +0100

    add Bong functionality

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,9 +1,10 @@
 ﻿for ( int i = 1; i < 106; i++ )
 {
     string output = "";
-    if (i % 3 == 0) output = output + "Fizz";
-    if (i % 5 == 0) output = output + "Buzz";
-    if (i % 7 == 0) output = output + "Bang";    
+    if ( i % 3 == 0 ) output = output + "Fizz";
+    if ( i % 5 == 0 ) output = output + "Buzz";
+    if ( i % 7 == 0 ) output = output + "Bang";
+    if ( i % 11 == 0 ) output = "Bong";
     if (output == "") output = i.ToString();
     Console.WriteLine(output);
 }
