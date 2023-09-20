f5fc89491907d7d5332e7372d5db782b8f1f5f01
Mark Roberts <Mark.Roberts@softwire.com>
2023-09-20 09:47:00 +0100

    initial functionality

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,8 @@
+﻿for (int i = 1; i< 21; i++)
+{
+    string output = "";
+    if (i % 3 == 0) output = output + "Fizz";
+    if (i % 5 == 0) output = output + "Buzz";
+    if (output == "") output = i.ToString();
+    Console.WriteLine(output);
+}
