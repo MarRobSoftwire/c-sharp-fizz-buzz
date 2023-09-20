b764c4222bd115adb458dbe1ff615c57bb36038a
Mark Roberts <Mark.Roberts@softwire.com>
2023-09-20 16:38:44 +0100

    add IEnumerable FizzBuzz

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,36 +1,116 @@
-﻿using System.Runtime.InteropServices;
-using System.Collections.Generic;
+﻿using System.Collections;
 
-for ( int i = 1; i < 256; i++ )
+public class FizzBuzzer : IEnumerable
 {
-    List<string> output = new List<string>();
-    if ( i % 3 == 0 ) output.Add("Fizz");
-    if ( i % 5 == 0 ) output.Add("Buzz");
-    if ( i % 7 == 0 ) output.Add("Bang");
-    if ( i % 11 == 0 ) {
-        output.Clear();
-        output.Add("Bong");
-    }
-    if ( i % 13 == 0 ) {
-        int firstBIndex = output.FindIndex( item => item.Substring(0,1) == "B");
-        if (firstBIndex == -1) {
-            output.Add("Fezz");
+    private string[] _values;
+
+    public FizzBuzzer(int maximum)
+    {
+        _values = new string[maximum];
+
+        for (int i=1; i < maximum+1; i++)
+        {
+            List<string> valueToAdd = new List<string>();
+        if ( i % 3 == 0 ) valueToAdd.Add("Fizz");
+        if ( i % 5 == 0 ) valueToAdd.Add("Buzz");
+        if ( i % 7 == 0 ) valueToAdd.Add("Bang");
+        if ( i % 11 == 0 ) {
+            valueToAdd.Clear();
+            valueToAdd.Add("Bong");
         }
-        else {
-            output.Insert(firstBIndex , "Fezz");
+        if ( i % 13 == 0 ) {
+            int firstBIndex = valueToAdd.FindIndex( item => item.Substring(0,1) == "B");
+            if (firstBIndex == -1) {
+                valueToAdd.Add("Fezz");
+            }
+            else {
+                valueToAdd.Insert(firstBIndex , "Fezz");
+            }
         }
-    }
-    if ( i % 17 == 0 ) {
-        output.Reverse();
+        if ( i % 17 == 0 ) {
+            valueToAdd.Reverse();
+        }
+
+        if (valueToAdd.Count == 0)
+        { 
+             _values[i-1] = Convert.ToString(i);
+        }
+        else
+        {
+            _values[i-1] = string.Join("",valueToAdd);
+        }
+           
+        }
     }
 
-    if (output.Count == 0)
-    { 
-        Console.WriteLine(i);
-    }
-    else
+    IEnumerator IEnumerable.GetEnumerator()
     {
-        Console.WriteLine(string.Join("",output));
+       return (IEnumerator) GetEnumerator();
     }
-    
+
+    public ValueEnum GetEnumerator()
+    {
+        return new ValueEnum(_values);
+    }
+}
+
+public class ValueEnum : IEnumerator
+{
+    public string[] _values;
+
+    int position = -1;
+
+    public ValueEnum(string[] list)
+    {
+        _values = list;
+    }
+
+    public bool MoveNext()
+    {
+        position++;
+        return (position < _values.Length);
+    }
+
+    public void Reset()
+    {
+        position = -1;
+    }
+
+    object IEnumerator.Current
+    {
+        get
+        {
+            return Current;
+        }
+    }
+
+    public string Current
+    {
+        get
+        {
+            try
+            {
+                return _values[position];
+            }
+            catch (IndexOutOfRangeException)
+            {
+                throw new InvalidOperationException();
+            }
+        }
+    } 
+}
+
+
+class App
+{
+    static void Main()
+    {
+        var fizzBuzzer = new FizzBuzzer(100);
+
+        foreach (string value in fizzBuzzer)
+        {
+            Console.WriteLine(value);
+        }
+
+    }
 }
