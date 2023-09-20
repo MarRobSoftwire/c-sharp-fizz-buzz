1abee21c95f2ce111c0554c0fd1b6fc3816b1029
Mark Roberts <Mark.Roberts@softwire.com>
2023-09-20 12:16:10 +0100

    add Fezz functionality and refactor to use Lists

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,10 +1,34 @@
-﻿for ( int i = 1; i < 106; i++ )
+﻿using System.Runtime.InteropServices;
+using System.Collections.Generic;
+
+for ( int i = 1; i < 14; i++ )
 {
-    string output = "";
-    if ( i % 3 == 0 ) output = output + "Fizz";
-    if ( i % 5 == 0 ) output = output + "Buzz";
-    if ( i % 7 == 0 ) output = output + "Bang";
-    if ( i % 11 == 0 ) output = "Bong";
-    if (output == "") output = i.ToString();
-    Console.WriteLine(output);
+    List<string> output = new List<string>();
+    if ( i % 3 == 0 ) output.Add("Fizz");
+    if ( i % 5 == 0 ) output.Add("Buzz");
+    if ( i % 7 == 0 ) output.Add("Bang");
+    if ( i % 11 == 0 ) {
+        output.Clear();
+        output.Add("Bong");
+    }
+    if ( i % 13 == 0 ) {
+        int firstBIndex = output.FindIndex( item => item.Substring(0,1) == "B");
+        if (firstBIndex == -1) {
+            // No B word found
+            output.Add("Fezz");
+        }
+        else {
+            // place before B word
+            output.Insert(firstBIndex , "Fezz");
+        }
+    }
+    if (output.Count == 0)
+    { 
+        Console.WriteLine(i);
+    }
+    else
+    {
+        Console.WriteLine(string.Join("",output));
+    }
+    
 }
